0df2619106a34ad655adbf94fbe2034f4a5c23c4
Rodgers Ouma <rodgers.ouma@twiga.com>
2022-03-05 17:23:23 +0300

    Updated README and tweak a few colours

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ var darkButton = `<svg width="32" height="32" fill="none">
                 </linearGradient>
             </defs>
         </svg>`
-var lightButton = `<svg width="24" height="24" fill="currentColor" class="transition-colors duration-200 text-gray-300">
+var lightButton = `<svg width="24" height="24" fill="currentColor" class="transition-colors duration-200">
             <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M9.353 2.939a1 1 0 01.22 1.08 8 8 0 0010.408 10.408 1 1 0 011.301 1.3A10.003 10.003 0 0112 22C6.477 22 2 17.523 2 12c0-4.207 2.598-7.805 6.273-9.282a1 1 0 011.08.22z">
             </path>
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-let tailwindConfig = process.env.HUGO_TAILWIND_CONFIG_JS || './tailwind.config.js';
-const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
   // eslint-disable-next-line no-process-env
-  plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])]
+  plugins: process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : []
 };
